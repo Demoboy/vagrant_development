bf771a511fcbfe4e547a0c0e61526ecea9527172
Kaelin Jacobson <kaelinjacobson@gmail.com>
2017-07-08 15:41:14 -0700

    Updated to prevent issue where composer folder wasn't always available

--- install.sh
+++ install.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-if [  -f ./install.sh ]; then
-    echo "Cannot run script from repo folder"
-    exit 1
-fi
-
-PROJECT_DIR="dev"
-
-if [[ $# -eq 1 ]] ; then
-    PROJECT_DIR="$1"
-fi
-
-if [ -d "$PROJECT_DIR" ]; then
-    echo "Cannot create project. Project with name already exists"
-    exit 1
-fi
-
-echo "Creating project in folder $1"
-
-mkdir $PROJECT_DIR
-
-echo "Cloning the development template"
-git clone https://github.com/128keaton/vagrant_development $PROJECT_DIR/dev/ --quiet
-
-echo "Cleaning up the mess we made"
-mv $PROJECT_DIR/dev/.git $PROJECT_DIR/dev/.git.old
-rm $PROJECT_DIR/dev/install.sh 
-rm $PROJECT_DIR/dev/README.md
 
 echo "Performing final setup"
 
@@ -45,7 +18,6 @@ fi
 
 if ! [ -f $PROJECT_DIR/Vagrantfile ]; then
     cat > $PROJECT_DIR/Vagrantfile <<EOF
-VAGRANT_DOTFILE_PATH = '.vagrant';
 require_relative 'dev/vagrant/vagrantfile.dist';
 EOF
 fi
